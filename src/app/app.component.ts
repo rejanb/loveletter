4a8c8d7b7567ad95529ba602b78db66288fe5df1
rejanb <rejanb06@gmail.com>
2021-04-02 12:38:28 +0545

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'love-calculator';
+  isShown: boolean = false; // hidden by default
+  isform: boolean = true;
+
+  name = '';
+  year = '';
+  favColour = '';
+  loveAbout = '';
+  randomItem = '';
+
+  clear() {
+    (<HTMLInputElement>document.getElementById('name')).value = '';
+    (<HTMLInputElement>document.getElementById('year')).value = '';
+    (<HTMLInputElement>document.getElementById('favColour')).value = '';
+    (<HTMLInputElement>document.getElementById('loveabout')).value = '';
+  }
+
+  toggleShow() {
+    
+      this.isShown = !this.isShown;
+      this.isform = !this.isform;
+      this.name = (<HTMLInputElement>document.getElementById('name')).value;
+      this.year = (<HTMLInputElement>document.getElementById('year')).value;
+      this.favColour = (<HTMLInputElement>(
+        document.getElementById('favColour')
+      )).value;
+      this.loveAbout = (<HTMLInputElement>(
+        document.getElementById('loveabout')
+      )).value;
+
+      //add the letters here
+      var myArray = [
+        'His ' +
+          this.loveAbout +
+          'is infectious, that is what I love bout You. ',
+        // "Bananas"+ this.favColour, 
+        // "Pears" +this.loveAbout
+      ];
+  }
+
+  return() {
+    this.isShown = !this.isShown;
+    this.isform = !this.isform;
+  }
 }
